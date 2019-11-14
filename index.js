7562425d9ddd7229017d4e547c6419bb7b4a0609
flavioleans <leans.flavio@gmail.com>
2019-11-13 22:57:45 -0300

    new project register

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express();
+const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
+
+//config template Egine handlebars
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebears')
+
+//connection DB
+const sequelize = new Sequelize('register_users', 'root', 'admin', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+//Rotas
+app.get('/register', function(res, req){
+    res.setEncoding("")
+})
+
+const Post = sequelize.define('posts', {
+    title: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.TEXT
+    }
+})
+
+const User = sequelize.define('users', {
+    name: {
+        type: Sequelize.STRING
+    },
+    lastname: {
+        type: Sequelize.STRING
+    },
+    age: {
+        type: Sequelize.INTEGER
+    },
+    mail: {
+        type: Sequelize.STRING
+    }
+})
+
+app.listen(8081, function(){
+    console.log("connected to server http://localhost:8081")
+})
